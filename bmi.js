9dc9f783348e532a6cb8682e850bc6ca074fada0
setayeshtaherzadeh <107579513+setayeshtaherzadeh@users.noreply.github.com>
2023-06-18 15:08:24 +0330

    Create bmi.js

---
+++ bmi.js
@@ -0,0 +1,55 @@
+// mikhaim zamani bmi mohasebe beshe ke yeki az input hamon change beshe 
+//on 2 ta input range ro tarif mikonim to js 
+//onchange tarif mikonim bara 2 tashun ke tariq kardan felan beshe
+
+// value bmi ro miaim dakhele tag p qarar midim 
+// vaqti dastgiire ro tekon midim tavelesh nakoni adada taqir nmikonan va in bade
+// pas miaim be ja roidad onchange roidad oninput mizarim
+//bmi to baze haie khasi bud rang neveshte va mstne paine adad baiad taqiir kone 
+
+
+
+const weightInput = document.querySelector('#weight')
+const heightInput = document.querySelector('#height')
+const weightVal = document.querySelector('#weight-val')   //1
+const heightVal = document.querySelector('#height-val')   //1
+const bmiResult = document.querySelector('#result')  
+const resultText = document.querySelector('#category')
+
+function bmiCalculator(){
+    let weightInputValue = weightInput.value
+    let heightInputValue = heightInput.value
+    weightVal.innerHTML = weightInputValue                 //1 baraie inke ro input range karbar dastgire ro k jabe ja kard ro har adadi vaisad adaddi 
+    heightVal.innerHTML = heightInputValue                //1  ; on baqal neshun dade mishe ham taqir kone 
+
+    // formole mohasebe : ma qad ro be cm gereftim vali b m mikhaim 
+
+    let bmiValue = (weightInputValue / (Math.pow(heightInputValue/100 ,2))).toFixed(1) // miad be m tabdil betavan 2 miresone  // miad 1 raqam ashar neshun mide
+
+    bmiResult.innerHTML = bmiValue    
+
+if(bmiValue < 18.5){
+resultText.innerHTML =" under weight"
+bmiResult.style.color = '#ffc44d'
+
+} else if (bmiValue >= 18.5 && bmiValue <= 24.9){
+    resultText.innerHTML =" normal weight"
+    bmiResult.style.color = 'green'
+
+}else if( bmiValue >= 25 && bmiValue < 29.9){
+    resultText.innerHTML =" over weight"
+    bmiResult.style.color = '#ff884d'
+
+}else{
+    resultText.innerHTML ="obses"
+    bmiResult.style.color = '#ff5e4d'
+}
+
+
+}
+
+//weightInput.addEventListener('change',bmiCalculator)
+//heightInput.addEventListener('change',bmiCalculator)
+weightInput.addEventListener('input',bmiCalculator)
+heightInput.addEventListener('input',bmiCalculator)
+
